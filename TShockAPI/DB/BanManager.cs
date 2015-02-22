ee4366beace6e27acf17e404b1c707d7e098a68e
서버들 <willowseo@gmail.com>
2015-02-22 21:00:07 +0900

    SubChecked_01
    Project ReProducing

--- TShockAPI/DB/BanManager.cs
+++ TShockAPI/DB/BanManager.cs
@@ -20,234 +20,214 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using MySql.Data.MySqlClient;
-
 namespace TShockAPI.DB
 {
-	public class BanManager
-	{
-		private IDbConnection database;
-
-		public BanManager(IDbConnection db)
-		{
-			database = db;
-
-			var table = new SqlTable("Bans",
-			                         new SqlColumn("IP", MySqlDbType.String, 16) {Primary = true},
-			                         new SqlColumn("Name", MySqlDbType.Text),
-									 new SqlColumn("UUID", MySqlDbType.Text),
-			                         new SqlColumn("Reason", MySqlDbType.Text),
-                                     new SqlColumn("BanningUser", MySqlDbType.Text),
-                                     new SqlColumn("Date", MySqlDbType.Text),
-                                     new SqlColumn("Expiration", MySqlDbType.Text)
-				);
-			var creator = new SqlTableCreator(db,
-			                                  db.GetSqlType() == SqlType.Sqlite
-			                                  	? (IQueryBuilder) new SqliteQueryCreator()
-			                                  	: new MysqlQueryCreator());
-			try
-			{
-				creator.EnsureExists(table);
-			}
-			catch (DllNotFoundException)
-			{
-				System.Console.WriteLine("Possible problem with your database - is Sqlite3.dll present?");
-				throw new Exception("Could not find a database library (probably Sqlite3.dll)");
-			}
-		}
-
-		/// <summary>
-		/// Gets a ban by IP.
-		/// </summary>
-		/// <param name="ip">The IP.</param>
-		/// <returns>The ban.</returns>
-		public Ban GetBanByIp(string ip)
-		{
-			try
-			{
-				using (var reader = database.QueryReader("SELECT * FROM Bans WHERE IP=@0", ip))
-				{
-					if (reader.Read())
-						return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
-				}
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex.ToString());
-			}
-			return null;
-		}
-
-		/// <summary>
-		/// Gets a list of bans.
-		/// </summary>
-		public List<Ban> GetBans()
-		{
-			List<Ban> banlist = new List<Ban>();
-			try
-			{
-				using (var reader = database.QueryReader("SELECT * FROM Bans"))
-				{
-					while (reader.Read())
-					{
-						banlist.Add(new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration")));					
-					}
-					return banlist;
-				}
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex.ToString());
-				Console.WriteLine(ex.StackTrace);
-			}
-			return null;
-		}
-
-		/// <summary>
-		/// Gets a ban by name.
-		/// </summary>
-		/// <param name="name">The name.</param>
-		/// <param name="casesensitive">Whether to check with case sensitivity.</param>
-		/// <returns>The ban.</returns>
-		public Ban GetBanByName(string name, bool casesensitive = true)
-		{
-			try
-			{
-				var namecol = casesensitive ? "Name" : "UPPER(Name)";
-				if (!casesensitive)
-					name = name.ToUpper();
-				using (var reader = database.QueryReader("SELECT * FROM Bans WHERE " + namecol + "=@0", name))
-				{
-					if (reader.Read())
-						return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
-				}
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex.ToString());
-			}
-			return null;
-		}
-
-		/// <summary>
-		/// Gets a ban by UUID.
-		/// </summary>
-		/// <param name="uuid">The UUID.</param>
-		/// <returns>The ban.</returns>
-		public Ban GetBanByUUID(string uuid)
-		{
-			try
-			{
-				using (var reader = database.QueryReader("SELECT * FROM Bans WHERE UUID=@0", uuid))
-				{
-					if (reader.Read())
-						return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
-				}
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex.ToString());
-			}
-			return null;
-		}
-
+    public class BanManager
+    {
+        private IDbConnection database;
+        public BanManager(IDbConnection db)
+        {
+            database = db;
+            var table = new SqlTable("ss_tsBans",
+            new SqlColumn("IP", MySqlDbType.String, 16) { Primary = true },
+            new SqlColumn("Name", MySqlDbType.Text),
+            new SqlColumn("UUID", MySqlDbType.Text),
+            new SqlColumn("Reason", MySqlDbType.Text),
+            new SqlColumn("BanningUser", MySqlDbType.Text),
+            new SqlColumn("Date", MySqlDbType.Text),
+            new SqlColumn("Expiration", MySqlDbType.Text)
+            );
+            var creator = new SqlTableCreator(db,
+            db.GetSqlType() == SqlType.Sqlite
+            ? (IQueryBuilder)new SqliteQueryCreator()
+            : new MysqlQueryCreator());
+            try
+            {
+                creator.EnsureExists(table);
+            }
+            catch (DllNotFoundException)
+            {
+                System.Console.WriteLine("Possible problem with your database - is Sqlite3.dll present?");
+                throw new Exception("Could not find a database library (probably Sqlite3.dll)");
+            }
+        }
+        /// <summary>
+        /// Gets a ban by IP.
+        /// </summary>
+        /// <param name="ip">The IP.</param>
+        /// <returns>The ban.</returns>
+        public Ban GetBanByIp(string ip)
+        {
+            try
+            {
+                using (var reader = database.QueryReader("SELECT * FROM ss_tsBans WHERE IP=@0", ip))
+                {
+                    if (reader.Read())
+                        return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets a list of bans.
+        /// </summary>
+        public List<Ban> GetBans()
+        {
+            List<Ban> banlist = new List<Ban>();
+            try
+            {
+                using (var reader = database.QueryReader("SELECT * FROM ss_tsBans"))
+                {
+                    while (reader.Read())
+                    {
+                        banlist.Add(new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration")));
+                    }
+                    return banlist;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+                Console.WriteLine(ex.StackTrace);
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets a ban by name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="casesensitive">Whether to check with case sensitivity.</param>
+        /// <returns>The ban.</returns>
+        public Ban GetBanByName(string name, bool casesensitive = true)
+        {
+            try
+            {
+                var namecol = casesensitive ? "Name" : "UPPER(Name)";
+                if (!casesensitive)
+                    name = name.ToUpper();
+                using (var reader = database.QueryReader("SELECT * FROM ss_tsBans WHERE " + namecol + "=@0", name))
+                {
+                    if (reader.Read())
+                        return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets a ban by UUID.
+        /// </summary>
+        /// <param name="uuid">The UUID.</param>
+        /// <returns>The ban.</returns>
+        public Ban GetBanByUUID(string uuid)
+        {
+            try
+            {
+                using (var reader = database.QueryReader("SELECT * FROM ss_tsBans WHERE UUID=@0", uuid))
+                {
+                    if (reader.Read())
+                        return new Ban(reader.Get<string>("IP"), reader.Get<string>("Name"), reader.Get<string>("UUID"), reader.Get<string>("Reason"), reader.Get<string>("BanningUser"), reader.Get<string>("Date"), reader.Get<string>("Expiration"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return null;
+        }
 #if COMPAT_SIGS
-		[Obsolete("This method is for signature compatibility for external code only")]
-		public bool AddBan(string ip, string name, string reason)
-		{
-			return AddBan(ip, name, "", reason, false, "", "");
-		}
+[Obsolete("This method is for signature compatibility for external code only")]
+public bool AddBan(string ip, string name, string reason)
+{
+return AddBan(ip, name, "", reason, false, "", "");
+}
 #endif
-		public bool AddBan(string ip, string name = "", string uuid = "", string reason = "", bool exceptions = false, string banner = "", string expiration = "")
-		{
-			try
-			{
-				return database.Query("INSERT INTO Bans (IP, Name, UUID, Reason, BanningUser, Date, Expiration) VALUES (@0, @1, @2, @3, @4, @5, @6);", ip, name, uuid, reason, banner, DateTime.UtcNow.ToString("s"), expiration) != 0;
-			}
-			catch (Exception ex)
-			{
-				if (exceptions)
-					throw ex;
-				Log.Error(ex.ToString());
-			}
-			return false;
-		}
-
+        public bool AddBan(string ip, string name = "", string uuid = "", string reason = "", bool exceptions = false, string banner = "", string expiration = "")
+        {
+            try
+            {
+                return database.Query("INSERT INTO ss_tsBans (IP, Name, UUID, Reason, BanningUser, Date, Expiration) VALUES (@0, @1, @2, @3, @4, @5, @6);", ip, name, uuid, reason, banner, DateTime.UtcNow.ToString("s"), expiration) != 0;
+            }
+            catch (Exception ex)
+            {
+                if (exceptions)
+                    throw ex;
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
 #if COMPAT_SIGS
-		[Obsolete("This method is for signature compatibility for external code only")]
-		public bool RemoveBan(string ip)
-		{
-			return RemoveBan(ip, false, true, false);
-		}
+[Obsolete("This method is for signature compatibility for external code only")]
+public bool RemoveBan(string ip)
+{
+return RemoveBan(ip, false, true, false);
+}
 #endif
-		public bool RemoveBan(string match, bool byName = false, bool casesensitive = true, bool exceptions = false)
-		{
-			try
-			{
-				if (!byName)
-					return database.Query("DELETE FROM Bans WHERE IP=@0", match) != 0;
-
-				var namecol = casesensitive ? "Name" : "UPPER(Name)";
-				return database.Query("DELETE FROM Bans WHERE " + namecol + "=@0", casesensitive ? match : match.ToUpper()) != 0;
-			}
-			catch (Exception ex)
-			{
-				if (exceptions)
-					throw ex;
-				Log.Error(ex.ToString());
-			}
-			return false;
-		}
-
-		public bool ClearBans()
-		{
-			try
-			{
-				return database.Query("DELETE FROM Bans") != 0;
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex.ToString());
-			}
-			return false;
-		}
-	}
-
-	public class Ban
-	{
-		public string IP { get; set; }
-
-		public string Name { get; set; }
-
-		public string UUID { get; set; }
-
-		public string Reason { get; set; }
-
+        public bool RemoveBan(string match, bool byName = false, bool casesensitive = true, bool exceptions = false)
+        {
+            try
+            {
+                if (!byName)
+                    return database.Query("DELETE FROM ss_tsBans WHERE IP=@0", match) != 0;
+                var namecol = casesensitive ? "Name" : "UPPER(Name)";
+                return database.Query("DELETE FROM Bans WHERE " + namecol + "=@0", casesensitive ? match : match.ToUpper()) != 0;
+            }
+            catch (Exception ex)
+            {
+                if (exceptions)
+                    throw ex;
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
+        public bool ClearBans()
+        {
+            try
+            {
+                return database.Query("DELETE FROM ss_tsBans") != 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
+    }
+    public class Ban
+    {
+        public string IP { get; set; }
+        public string Name { get; set; }
+        public string UUID { get; set; }
+        public string Reason { get; set; }
         public string BanningUser { get; set; }
-
         public string Date { get; set; }
-
         public string Expiration { get; set; }
-
-		public Ban(string ip, string name, string uuid, string reason, string banner, string date, string exp)
-		{
-			IP = ip;
-			Name = name;
-			UUID = uuid;
-			Reason = reason;
-		    BanningUser = banner;
-		    Date = date;
-		    Expiration = exp;
-		}
-
-		public Ban()
-		{
-			IP = string.Empty;
-			Name = string.Empty;
-			UUID = string.Empty;
-			Reason = string.Empty;
-		    BanningUser = "";
-		    Date = "";
-		    Expiration = "";
-		}
-	}
+        public Ban(string ip, string name, string uuid, string reason, string banner, string date, string exp)
+        {
+            IP = ip;
+            Name = name;
+            UUID = uuid;
+            Reason = reason;
+            BanningUser = banner;
+            Date = date;
+            Expiration = exp;
+        }
+        public Ban()
+        {
+            IP = string.Empty;
+            Name = string.Empty;
+            UUID = string.Empty;
+            Reason = string.Empty;
+            BanningUser = "";
+            Date = "";
+            Expiration = "";
+        }
+    }
 }
